557984288898b1049ae86429e84c4011ff0f2f67
Sambat-Basnet <sambatbasnet81@gmail.com>
2021-08-24 11:51:33 +0545

    Made UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import {nanoid} from 'nanoid';
-import {Itasks,typeOfArr} from './types'
-
-
+import {Itasks,typeOfArr,daysArr, monthArr} from './types'
+import {AiOutlineCheck,AiFillCheckCircle} from 'react-icons/ai'
+import {BsTrash,BsArrowCounterclockwise} from 'react-icons/bs'
+import {BiCheck} from 'react-icons/bi'
 const App:React.FC = () => {
   // const[count, setCount] = useState<number>(0)
   // const[word,setWord] = useState<string>("")
   const[task, setTask] = useState<Itasks>({id:'', value: '',isComplete:false})
   const[arr,setArr] = useState<typeOfArr>([])
+  const[selected, setSelected] = useState<boolean>(false)
 
-  useEffect(()=>{
-    console.log('Hello')
-  },[task])
-
+  // useEffect(()=>{
+  //   console.log('Hello')
+  // },[task])
   const handleInputChange= (e:React.ChangeEvent<HTMLInputElement>) => {
     setTask({...task,id:nanoid(),value:e.target.value})
   }
@@ -25,33 +26,58 @@ const App:React.FC = () => {
     else{
     setArr([...arr, task])
     setTask({id:'', value: '',isComplete:false})
+    setSelected(false)
     }
   }
+  let days:daysArr=['Sunday', 'Monday', 'Tuesday','Wednesday','Thursday', 'Friday','Saturday'] 
+  let month:monthArr=['January','February', 'March','April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const indexOfDay = new Date().getDay()
+  const date = new Date().getDate()
+  const indexOfMonth = new Date().getMonth()
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
+  <div className="main">
+    <div className="container-1">
+    <div className="dateSection">
+      <p className="styleDay">{days[indexOfDay]}</p>
+      <p className="styleDate">{date} {month[indexOfMonth]}</p>
+      </div>
+    <div className="container-2">
+      <button className="addBtn" onClick={()=>{
+        setSelected(true)
+      }}>+</button>
+      {selected && (<form onSubmit={handleFormSubmit}>
+        <div className="addingTask">
       <input type="text" value={task.value} placeholder="Enter your daily task" 
       onChange={handleInputChange}/>
-      <button type="submit">Click to Add</button>
-      </form>
+      <button className="taskBtn1" type="submit">+</button>
+      <button onClick={()=>{
+        setSelected(false)
+      }}className="taskBtn2" type="submit">&#215;</button>
+      </div>
+      </form>)}
+      <div className="taskItems">
       <ul>
         {arr.map(({id,value,isComplete})=>(
-          <div key={id}>
+          <div className="indi" key={id}>
           <li>{value}</li>
-          <button onClick={()=>{
+          <button className="check"  onClick={()=>{
             //delete garyo feri tei append garyo with reversing the value of isComplete
             setArr([...arr.filter(x=>x.id!=id),{id,value,isComplete:!isComplete}])
           }
-            }>{isComplete?'Do It Again':'Done'}</button>
-            <button onClick={
+            }>{isComplete?
+            <BsArrowCounterclockwise/>:<BiCheck/>}</button>
+            <button className="trashCan" onClick={
               ()=>{
                 //iterates each member of an array and filters out the chosen member
                 setArr(arr.filter(x=>x.id!=id))
               }
-            }>Delete</button>
+            }><BsTrash/></button>
           </div>
         ))}
       </ul>
+      </div>
+      </div>
+    </div>
     </div>
   )
 }
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,6 @@ export interface Itasks{
 }
 
 export type typeOfArr = Array<Itasks>
+export type daysArr = Array<string>
+export type monthArr = Array<string>
+export type newArr = Array<string>
